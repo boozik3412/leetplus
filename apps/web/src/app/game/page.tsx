bf2faf6fe9549bf4bfb14e57bb7d087d12690f01
boozik3412 <nikita_gorenkov@mail.ru>
2026-06-30 19:04:06 +0500

    feat: add public game route

---
+++ apps/web/src/app/game/page.tsx
@@ -0,0 +1,7 @@
+import { GameSummaryClient } from "../play/game/game-summary-client";
+
+export const dynamic = "force-dynamic";
+
+export default function GamePage() {
+  return <GameSummaryClient />;
+}

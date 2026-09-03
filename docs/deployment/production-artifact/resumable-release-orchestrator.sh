c4d855873b2272b08c2629af769868cdb552073b
boozik3412 <nikita_gorenkov@mail.ru>
2026-09-03 11:40:35 +0500

    fix(release): pin hardened orchestrator engine

--- docs/deployment/production-artifact/resumable-release-orchestrator.sh
+++ docs/deployment/production-artifact/resumable-release-orchestrator.sh
@@ -51,7 +51,7 @@ umask 0077
 
 readonly INSTALLED_BOOTSTRAP='/usr/local/sbin/leetplus-resumable-release-orchestrator'
 readonly INSTALLED_ENGINE='/usr/local/libexec/leetplus/resumable-release-orchestrator.mjs'
-readonly EXPECTED_ENGINE_SHA256='42151d9e7cf870cc4b3de49ff0bda2492bd0227934569ab0ad8f317d0d24b2d2'
+readonly EXPECTED_ENGINE_SHA256='d602e46028fcaf8b6bb15363685bfb693b92eae2f579ba291ceb82a94357b01a'
 readonly PRODUCTION_CONTROL_RUN_ROOT='/run/leetplus-production-control'
 readonly PRODUCTION_CONTROL_INSTALL_LOCK="${PRODUCTION_CONTROL_RUN_ROOT}/install.lock"
 readonly STATE_PARENT='/var/lib/leetplus/deploy-receipts'
